abccba592d751728b1a2d4a8a1a8703626b8c20e
Prarthana <prarthanashr04@gmail.com>
2020-09-17 18:31:47 +0530

    changed actual slots

--- src/Components/Actualslots.js
+++ src/Components/Actualslots.js
@@ -1,6 +1,19 @@
 import React from 'react';
+
+const configTask = (props) =>{
+    return(
+        <div className="inlineclass" >
+            <div>{props.taskName}</div>
+            <Button></Button>
+        </div>
+    );
+}
 const ActualSlots = (props) => {
     return (<div>
+        <h1>Actual Slots</h1>
+        {props.config[0]}
+        {props.config[1]}
+          
 </div>);
 }
 
