4db5a79975a9e6db61699da72769afdfaa294d23
Ritik98 <55049886+Ritik98@users.noreply.github.com>
2020-09-19 16:15:23 +0530

    Update Actualslots.js

--- src/Components/Actualslots.js
+++ src/Components/Actualslots.js
@@ -1,19 +1,36 @@
 import React from 'react';
-
-const configTask = (props) =>{
-    return(
-        <div className="inlineclass" >
-            <div>{props.taskName}</div>
-            <Button></Button>
-        </div>
-    );
+import { Button } from 'reactstrap';
+function submit(){
+    alert();
 }
 const ActualSlots = (props) => {
     return (<div>
-        <h1>Actual Slots</h1>
-        {props.config[0]}
-        {props.config[1]}
-          
+        <div>
+            <h1>Actual slots</h1>
+        </div>
+        <div className="inlineclass">
+        <h3 className="col">{props.config[0].name}</h3>
+        <h3 className="col">{props.config[0].Slots}</h3>
+        <input className="col" type="number" min="1" max="10" required/>
+        </div>
+        <br />
+        <div className="inlineclass">
+        <h3 className="col">{props.config[1].name}</h3>
+        <h3 className="col">{props.config[1].Slots}</h3>
+        <input className="col" type="number" min="1" max="10" required/>
+        </div>
+        <br />
+        <div className="inlineclass">
+           <h3> Notes </h3>
+           <textarea rows="4" cols="50" name="comment"></textarea>
+        </div >
+        <br/>
+        <div className="inlineclass">
+        <Button onClick={submit}>Submit</Button>
+        </div >
+        
+        
+        
 </div>);
 }
 
