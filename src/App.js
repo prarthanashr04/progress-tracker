dfe195ddae122d730927572d91970e6a67f88d82
Prarthana <prarthanashr04@gmail.com>
2020-09-20 19:36:07 +0530

    slot's interface changed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import './App.css';
 import Header from './Components/Navbar';
 import Dat from './Components/Date';
 import PlannedSlots from './Components/Plannedslots';
 import ActualSlots from './Components/Actualslots';
 
-function App() {
-  return (<div className="App">
-    <Header logo = './skillpill.png' title  = "progress tracker"  name = "Ritik"  />
-    <Dat />
-    <ActualSlots config={[{"name":"production","Slots":4},{"name":"core","Slots":2}]} />
-    <PlannedSlots />
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      member:{
+        "name":"",
+        "previous":{}
+      }
+    }
+  }
+
+  
+  render(){
+    document.body.style.backgroundImage = "url('./bgimage.jpg')";
+  if(this.state.member.name==="")
+    {
+      var date,actual,planned
+      date=<Dat /> ;
+      actual=<ActualSlots config={[{"name":"production","Slots":4},{"name":"core","Slots":2}]} /> ;
+      planned=<PlannedSlots />
+      
+    }
+  return (<div className="App" >
+    <Header logo = './skillpill.png' title  = "progress tracker"   />
+    {date}{actual}{planned}
+    
    </div>   
   );
+  }
 }
 
 export default App;
