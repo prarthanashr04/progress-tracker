06b71cc506f509cf3cf894fdba7d766ab2523592
Prarthana <prarthanashr04@gmail.com>
2020-09-21 19:02:09 +0530

    changed css

--- src/Components/Actualslots.js
+++ src/Components/Actualslots.js
@@ -4,41 +4,38 @@ function submit() {
     alert();
 }
 const ActualSlots = (props) => {
-    return (<div>
-        <div className="col-12 row row-content offset-2">
+    return (<Form className="formslots ">
+        <div className="col-12 row offset-sm-3">
             <h1>Actual slots</h1>
         </div>
-        <div className="col-12 col-md-6">
-            <Form>
-                <Row className="form-group">
-                    <Label htmlFor="taskname" md={2}>{props.config[0].name}</Label>
-                    <Label htmlFor="planslots" md={2}>{props.config[0].Slots}</Label>
-                    <input className="col" id="actualslot" placeholder="Slots Given" type="number" min="1" max="10" required />
-                
-                </Row>
+        <div className="col-12 col-md-10">
 
-                <Row className="form-group">
-                    <Label htmlFor="taskname" md={2}>{props.config[1].name}</Label>
-                    <Label htmlFor="taskslots" md={2}>{props.config[1].Slots}</Label>
-                    <input className="col"id="actualslot" placeholder="Slots Given" type="number" min="1" max="10" required />
-                </Row>
+            <Row className="form-group">
+                <Label htmlFor="taskname" md={2}>{props.config[0].name}</Label>
+                <Label htmlFor="planslots" md={2}>{props.config[0].Slots}</Label>
+                <input className="col" id="actualslot" placeholder="Slots Given" type="number" min="1" max="10" required />
 
-                <Row className="form-group">
-                    <Label htmlFor="message" md={2}>Notes </Label>
-                    <textarea rows="4" className="col" name="comment"></textarea>
-                </Row>
+            </Row>
 
-                <Row className="form-group">
-                    <Col md={{size: 10, offset: 2}}>
-                        <Button onClick={submit}>Submit</Button>
-                    </Col>
-                </Row>
-            </Form>
+            <Row className="form-group">
+                <Label htmlFor="taskname" md={2}>{props.config[1].name}</Label>
+                <Label htmlFor="taskslots" md={2}>{props.config[1].Slots}</Label>
+                <input className="col" id="actualslot" placeholder="Slots Given" type="number" min="1" max="10" required />
+            </Row>
+
+            <Row className="form-group">
+                <Label htmlFor="message" md={2}>Notes </Label>
+                <textarea rows="4" className="col" name="comment"></textarea>
+            </Row>
+
+            <Row className="form-group">
+                <Col md={{ size: 10, offset: 2 }}>
+                    <Button onClick={submit}>Submit</Button>
+                </Col>
+            </Row>
+
         </div>
-
-
-
-    </div>);
+    </Form>);
 }
 
 export default ActualSlots;
--- src/Components/Plannedslots.js
+++ src/Components/Plannedslots.js
@@ -1,38 +1,40 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import { Button,Form } from 'reactstrap';
 const PlanTask = (props) => {
-    if(props.showCross)
-    var cross=<Button>X</Button>;
+    if (props.showCross)
+        var cross = <Button>X</Button>;
     return (<div className="inlineclass">
-    <input className="col" type="text" placeholder="Task Name" required/>
-    <input className="col" type="number" placeholder="Planned Slots" required/>
-    {cross}
+        <input className="col" type="text" placeholder="Task Name" required />
+        <input className="col" type="number" placeholder="Planned Slots" required />
+        {cross}
     </div>)
 }
-var showCross=false,tasks=[<div><PlanTask showCross={showCross}/><br/></div>]
-function Add(){
+var showCross = false, tasks = [<div><PlanTask showCross={showCross} /><br /></div>]
+function Add() {
     alert();
-    showCross=true;
+    showCross = true;
     tasks.push(
-        <div><PlanTask showCross={showCross}/><br/></div>
+        <div><PlanTask showCross={showCross} /><br /></div>
     );
 }
 function submit() {
     alert();
 }
 const PlannedSlots = (props) => {
-    return (<div className="col-12 col-md-6">
-    <h1>Let's plan our day</h1>
-    <div className="inlineclass">
-        <h5 className="col">Task Name</h5>
-        <h5 className="col">No. of Slots</h5>
-    </div>
-    <form>
-    {tasks}
-    <Button color="primary" onClick={Add}>Add</Button>
-    <Button onClick={submit}>Submit</Button>
-    </form>
-</div>);
+    return (<Form className="formslots">
+        <div className="col-12 col-md-10">
+            <h1>Let's plan our day</h1>
+            <div className="inlineclass">
+                <h5 className="col">Task Name</h5>
+                <h5 className="col">No. of Slots</h5>
+            </div>
+            <form>
+                {tasks}
+                <Button color="primary" onClick={Add}>Add</Button>
+                <Button onClick={submit}>Submit</Button>
+            </form>
+        </div>
+    </Form>);
 }
 
 export default PlannedSlots;
