7ece633bd335d7cdb74ef1cfd34dc2d1eca521b0
Prarthana <prarthanashr04@gmail.com>
2020-10-01 21:31:26 +0530

    login dropdown

--- src/Components/Member.js
+++ src/Components/Member.js
@@ -1,16 +1,44 @@
-import React, { Component } from 'react';
-import {Button} from 'reactstrap';
+import React, { Component, useState } from 'react';
+import {Button, ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
+
 
 class Member extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            dropdownOpen : false,
+            
+        };
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle(){
+        this.setState({
+            dropdownOpen: !this.state.dropdownOpen
+          });
+    }
     render(){
         if(this.props.name==="")
         return(<div>
             <Button onClick={this.props.modalButton}>
                 Login
             </Button>
-        </div>)
+        </div>);
         else
-        return(<div id="name">{this.props.name}</div>)
+        return(<ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+            <DropdownToggle id="name" caret>
+            {this.props.name}
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem >Past Record</DropdownItem>
+              <DropdownItem >Consistency</DropdownItem>
+              <DropdownItem>Profile</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Log out</DropdownItem>
+            </DropdownMenu>
+          </ButtonDropdown>);
     }
 }
 export default Member;
