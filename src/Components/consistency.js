8ea7ec8a9917639636a102212f096af2a794e8a3
Jatin <adriano23012001@gmail.com>
2020-10-02 16:07:38 +0530

    updated notes

---
+++ src/Components/consistency.js
@@ -0,0 +1,12 @@
+import React,{Component } from 'react';
+import '../App.css';
+import { Button,Navbar} from 'reactstrap';
+
+class Consistency extends Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+export default Consistency;
---
+++ src/Components/pastRecord.js
@@ -0,0 +1,12 @@
+import React,{Component } from 'react';
+import '../App.css';
+import { Button,Navbar} from 'reactstrap';
+
+class PastRecord extends Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+export default PastRecord;
