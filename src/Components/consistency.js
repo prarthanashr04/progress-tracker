5b949cfedce6b9864aca0197eadf628d01043954
Jatin <adriano23012001@gmail.com>
2020-10-10 13:29:37 +0530

    DailyPlan completed

--- src/Components/consistency.js
+++ src/Components/consistency.js
@@ -4,6 +4,9 @@ import { Button,Navbar} from 'reactstrap';
 
 class Consistency extends Component{
     render(){
+        var week={"Core":2,"Production":4};
+        var month={"Core":2,"Production":4};
+        var quarter={"Core":2,"Production":4};
         return(
             <div></div>
         )
--- src/Components/dailyPlan.js
+++ src/Components/dailyPlan.js
@@ -6,20 +6,78 @@ import PlannedSlots from './Plannedslots';
 import ThisDate from './Date';
 import ActualSlots from './Actualslots';
 class DailyPlan extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isActual: false,
+            data: {},
+            isPlanned:true,
+            plannedData:{}
+        }
+        this.onSubmit=this.onSubmit.bind(this);
+    }
+    componentDidMount() {
+        var cDate = new Date();
+        var timeInMilliSeconds = cDate.getTime();
+        var timeInSeconds = Math.floor(timeInMilliSeconds / 1000);
+        var hrs = cDate.getHours();
+        var min = cDate.getMinutes();
+        var sec = cDate.getSeconds();
+        var hrsMinsInSecs = (hrs * 60 * 60) + (min * 60) + sec;
+        var dateInSeconds = timeInSeconds - hrsMinsInSecs-86400;
+        console.log(dateInSeconds);
+        var apiUrl = "https://mj3a9u0swa.execute-api.ap-south-1.amazonaws.com/dev?UserName=" + this.props.name + "&Date=" + dateInSeconds;
+        fetch(apiUrl)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    if(result.isActual)
+                    this.setState({
+                        isActual: true,
+                        data: result.actual[0]
+                    });
+                    if(!result.isPlanned){
+                        this.setState({
+                            isPlanned:false,
+                            plannedData:result.planned[0]
+                        })
+                    }
+                }
+            )
+    }
+    
+    onSubmit(){
+        var output=false;
+        output=this.props.submit(this.state.isActual);
+        if(output!==false){
+            var data={
+                "Core":output[0],
+                "Production":output[1]
+            }   
+            this.setState({
+                isActual:false,
+                isPlanned:false,
+                plannedData:data
+            });
+        }
+    }
     render() {
+        var actual,submit;
+        if(this.state.isActual)
+        actual=<ActualSlots data={this.state.data} />;
+        if(this.state.isPlanned)
+        submit=<Submit onClick={this.onSubmit}/>;
         return (
             <div>
                 <ThisDate />;
                 <div className="row ">
+                {actual}
                     <div className="parentform">
-                        <ActualSlots apiUrl="https://mj3a9u0swa.execute-api.ap-south-1.amazonaws.com/dev" userName={this.props.name} />;
-
+                        <PlannedSlots config={["Core", "Production"]} planned={this.state.isPlanned} data={this.state.plannedData}/>
                     </div>
-                    <div className="parentform">
-                        <PlannedSlots config={["Core", "Production"]} />
-                    </div>
+                    {submit}
                 </div>
-                <Submit onClick={this.submit} />
             </div>
         )
     }
