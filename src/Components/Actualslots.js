9844749ff5535c7f1959d9a810ca38fb98722c2c
Jatin <adriano23012001@gmail.com>
2020-11-13 12:34:33 +0530

    Revert to first release

--- src/Components/Actualslots.js
+++ src/Components/Actualslots.js
@@ -4,7 +4,7 @@ import { Form, Row, Label } from 'reactstrap';
 class ActualSlots extends Component {
     
     render() {
-        return (<div className="col-sm-12 col-md-6"><Form className="formslots ">
+        return (<div className="parentform"><Form className="formslots ">
             <div className="col-12 row offset-sm-3">
                 <h1>Actual slots</h1>
             </div>
